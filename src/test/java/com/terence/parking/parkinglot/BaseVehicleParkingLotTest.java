507013c0d200b7a384aeb7770833f5b496627eda
Terence Lim Say Jian <terence.lim@go-jek.com>
2021-04-23 00:05:21 +0800

    [Terence] Move test cases into base vehicle parking lot class

---
+++ src/test/java/com/terence/parking/parkinglot/BaseVehicleParkingLotTest.java
@@ -0,0 +1,64 @@
+package com.terence.parking.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseVehicleParkingLotTest {
+
+  @Test
+  void shouldReturnIdOfCarWhenParkedSuccessfully() throws Exception {
+    BaseVehicleParkingLot carVehicleParkingLot = new BaseVehicleParkingLot(3, "CarLot", VehicleType.CAR);
+
+    String firstParkedCarSpace = carVehicleParkingLot.park("SGX1234A", "1613541902");
+
+    assertEquals("CarLot1", firstParkedCarSpace);
+  }
+
+  @Test
+  void shouldReturnIdOfSecondCarWhenParkedSuccessfully() throws Exception {
+    BaseVehicleParkingLot carVehicleParkingLot = new BaseVehicleParkingLot(3, "CarLot", VehicleType.CAR);
+
+    String firstParkedCarSpace = carVehicleParkingLot.park("SGX1234A", "1613541902");
+    String secondParkedCarSpace = carVehicleParkingLot.park("WWW5555A", "1613541902");
+
+    assertEquals("CarLot1", firstParkedCarSpace);
+    assertEquals("CarLot2", secondParkedCarSpace);
+  }
+
+  @Test
+  void shouldThrowCarParkFullExceptionIfCarLotsAreFull() throws Exception {
+    BaseVehicleParkingLot carVehicleParkingLot = new BaseVehicleParkingLot(1, "CarLot", VehicleType.CAR);
+
+    String firstParkedCarSpace = carVehicleParkingLot.park("SGX1234A", "1613541902");
+    assertThrows(
+        CarParkFullException.class,
+        () -> carVehicleParkingLot.park("WWW5555A", "1613541902"));
+
+    assertEquals("CarLot1", firstParkedCarSpace);
+  }
+
+  @Test
+  void shouldReturnIdIdOfSecondCarWhenParkedSuccessfully() throws Exception {
+    BaseVehicleParkingLot carVehicleParkingLot = new BaseVehicleParkingLot(3, "CarLot", VehicleType.CAR);
+
+    String firstParkedCarSpace = carVehicleParkingLot.park("SGX1234A", "1613541902");
+    String secondParkedCarSpace = carVehicleParkingLot.park("WWW5555A", "1613541902");
+
+    assertEquals("CarLot1", firstParkedCarSpace);
+    assertEquals("CarLot2", secondParkedCarSpace);
+  }
+
+  @Test
+  void shouldReturnTimestampWhenExitingCarpark() throws Exception {
+    BaseVehicleParkingLot carVehicleParkingLot = new BaseVehicleParkingLot(3, "CarLot", VehicleType.CAR);
+
+    String firstParkedCarSpace = carVehicleParkingLot.park("SGX1234A", "1613541902");
+    assertEquals("CarLot1", firstParkedCarSpace);
+
+    ParkingSpotInfo parkingSpotInfo = carVehicleParkingLot.exit("SGX1234A");
+    assertEquals("1613541902", parkingSpotInfo.getTimestamp());
+    assertEquals("CarLot1", parkingSpotInfo.getLotId());
+  }
+
+}
